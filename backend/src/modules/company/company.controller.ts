a040f368de0a81d4581c9a3c88b4ec3bb88f8454
Onur Yasin <onuryasin1999@gmail.com>
2025-10-19 01:39:47 +0300

    401 hatası çözüldü

--- backend/src/modules/company/company.controller.ts
+++ backend/src/modules/company/company.controller.ts
@@ -1,15 +1,21 @@
 import { Controller, Post, Body, Get, Param, UseGuards, Req } from '@nestjs/common';
 import { CompanyService } from './company.service';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { CreateCompanyDto } from './dto/createCompany.dto';
 
 @UseGuards(JwtAuthGuard)
+@ApiBearerAuth('access-token')
+@ApiTags('companies')
 @Controller('companies')
 export class CompanyController {
   constructor(private companyService: CompanyService) {}
 
   @Post()
-  async createCompany(@Body() body: { name: string; domain: string }, @Req() req: any) {
-    const company = await this.companyService.createCompany(body.name, body.domain);
+  async createCompany(@Body() createCompanyDto: CreateCompanyDto, @Req() req: any) {
+    console.log('req.user:', req.user);
+    console.log('req.create:', createCompanyDto);
+    const company = await this.companyService.createCompany(createCompanyDto.name, createCompanyDto.domain);
     // Oluşturan kullanıcıyı otomatik ekle (admin)
     await this.companyService.addUserToCompany(req.user.userId, company.id, 'admin');
     return company;
--- backend/src/modules/company/dto/createCompany.dto.ts
+++ backend/src/modules/company/dto/createCompany.dto.ts
@@ -1,4 +1,13 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty } from "class-validator";
+
 export class CreateCompanyDto {
+  @ApiProperty({ example: 'Koçak Park' })
+  @IsNotEmpty()
   name: string;
+  
+  @ApiProperty({ example: 'kocakPark' })
+  @IsNotEmpty()
   domain: string;
+  
 }
